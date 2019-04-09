fec5dbe85adeb63ac82e7889da9287ff220198d0
Paule HAN <han.paule@gmail.com>
2019-04-09 21:47:51 +0000

    Added tasks

---
+++ 0010.js
@@ -0,0 +1,101 @@
+/*
+★★☆☆☆
+Дано объект. Написать функцию (Transform) которая создаст объект с другой структурой
+
+let source = {
+  ua: {
+    buttons: {
+      btn_open: "Відкрити",
+      btn_close: "Закрити",
+      btn_cancel: "Відміна",
+    },
+    texts: {
+      txt_ok: "Готово",
+      txt_not: "Не готово"
+    }
+  },
+  ru: {
+    buttons: {
+      btn_open: "Открыть",
+      btn_close: "Закрыть",
+      btn_cancel: "Отмена",
+    },
+    texts: {
+      txt_ok: "Ок",
+      txt_not: "Не ок"
+    }
+  }
+}
+console.log(Transform(source));
+
+Результатом выполнения должно быть:
+{
+  btn_open: {
+    ua: 'Відкрити',
+    ru: 'Открыть'
+  },
+  btn_close: {
+    ua: 'Закрити',
+    ru: 'Закрыть'
+  },
+  btn_cancel: {
+    ua: 'Відміна',
+    ru: 'Отмена'
+  },
+  txt_ok: {
+    ua: 'Готово',
+    ru: 'Ок'
+  },
+  txt_not: {
+    ua: 'Не готово',
+    ru: 'Не ок'
+  }
+}
+ */
+
+function Transform(source) {
+    
+  let res = {};
+  
+  for (let key in source) {
+      
+    for (let i in source[key]) {
+        
+      for (let o in source[key][i]) {
+  
+        if(!(o in res)) {
+            res[o] = {};
+        }
+        res[o][key] = source[key][i][o];
+      }
+    }
+  }
+
+  return res;
+}
+
+let source = {
+  ua: {
+    buttons: {
+      btn_open: "Відкрити",
+      btn_close: "Закрити",
+      btn_cancel: "Відміна",
+    },
+    texts: {
+      txt_ok: "Готово",
+      txt_not: "Не готово"
+    }
+  },
+  ru: {
+    buttons: {
+      btn_open: "Открыть",
+      btn_close: "Закрыть",
+      btn_cancel: "Отмена",
+    },
+    texts: {
+      txt_ok: "Ок",
+      txt_not: "Не ок"
+    }
+  }
+};
+console.log(Transform(source));
